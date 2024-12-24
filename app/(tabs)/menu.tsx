22fe60d48f7b9409ecd46b7d02b1a0d0bb302e9a
Manuel <manuel.t.shade@gmail.com>
2024-12-24 01:52:46 +0000

    feat: improved menu.tsx code

--- app/(tabs)/menu.tsx
+++ app/(tabs)/menu.tsx
@@ -1,15 +1,8 @@
 import { Colors } from "@/constants/Colors";
 import { useLanguage } from "@/context/Language";
 import { useTheme } from "@/context/Theme";
-import { useEffect, useState } from "react";
-import {
-  StyleSheet,
-  View,
-  Text,
-  FlatList,
-  Pressable,
-  Image,
-} from "react-native";
+import { useEffect, useMemo, useState } from "react";
+import { StyleSheet, View, Text, FlatList, Pressable } from "react-native";
 
 import { useRouter } from "expo-router";
 import { Category, fetchCategories } from "@/lib/sanity/httpSanity";
@@ -17,118 +10,83 @@ import { useDishes } from "@/context/Dishes";
 import Animated from "react-native-reanimated";
 import Constants from "expo-constants";
 
+const CARD_WIDTH = 550;
+const CARD_HEIGHT = 300;
+
 export default function MenuScreen() {
   const { theme } = useTheme();
   const { selectedLanguage } = useLanguage();
-  const [categories, setCategories] = useState<Category[]>([]);
   const { dishes } = useDishes();
   const router = useRouter();
-  Constants.platform;
-  const [selectedCategory, setSelectedCategory] = useState<number | undefined>(
-    undefined
-  );
-  const filteredDishes = [
-    ...dishes.filter((dish) => dish.categoryNumber === selectedCategory),
-  ];
 
-  console.log("COMPONENT RENDERED");
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState<
+    number | undefined
+  >();
+
+  const filteredDishes = useMemo(() => {
+    return dishes.filter((dish) => dish.categoryNumber === selectedCategory);
+  }, [dishes, selectedCategory]);
 
   const styles = createStyles(theme);
 
-  useEffect(() => {
-    fetchCategories().then((res) => {
-      setCategories(res.sort((a, b) => a.categoryNumber - b.categoryNumber));
-    });
-  }, []);
+  console.log("REDNER");
 
   useEffect(() => {
-    if (categories.length > 0) {
-      setSelectedCategory(categories[0].categoryNumber);
-    }
-  }, [categories]);
+    fetchCategories().then((res) => {
+      if (res.length === 0) return;
+      setCategories(res.sort((a, b) => a.categoryNumber - b.categoryNumber));
+      setSelectedCategory(res[0]?.categoryNumber);
+    });
+  }, []);
 
   return (
     <View style={styles.pageContainer}>
       {/*CATEGORIES_HEADER */}
-      <View
-        style={{
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
+      <View style={styles.categoriesWrapper}>
         <FlatList
-          contentContainerStyle={{
-            width: "100%",
-            alignItems: "center",
-            justifyContent: "space-around",
-          }}
-          style={{
-            backgroundColor: theme?.tint,
-            height: 100,
-            paddingVertical: 20,
-            borderBottomColor: "black",
-            borderBottomWidth: 2,
-          }}
+          contentContainerStyle={styles.categoriesContainer}
+          style={styles.categoriesList}
           data={categories}
-          renderItem={({ item }) => {
-            return (
-              <Pressable
-                onPress={() => setSelectedCategory(item.categoryNumber)}
-              >
-                <View
-                  style={{
-                    padding: 15,
-                    backgroundColor: theme?.background,
-                    minWidth: 100,
-                    alignItems: "center",
-                    justifyContent: "center",
-                    borderRadius: 10,
-                  }}
-                >
-                  <Text
-                    style={{
-                      color: theme?.text,
-                      textAlign: "center",
-                      fontSize: 20,
-                    }}
-                    key={item._id}
-                  >
-                    {item.title[selectedLanguage?.id as string] ??
-                      item.title.es}
-                  </Text>
-                </View>
-              </Pressable>
-            );
-          }}
+          renderItem={({ item }) => (
+            <Pressable onPress={() => setSelectedCategory(item.categoryNumber)}>
+              <View style={styles.categoryButton}>
+                <Text style={styles.categoryText} key={item._id}>
+                  {item.title[selectedLanguage?.id as string] ?? item.title.es}
+                </Text>
+              </View>
+            </Pressable>
+          )}
           keyExtractor={(item) => item._id}
           horizontal
         />
       </View>
-      {/* DISHES_LIST EXAMPLE */}
+      {/* DISHES_LIST */}
       <View style={styles.listContainer}>
         <FlatList
-          style={{ width: "100%" }}
-          contentContainerStyle={styles.flatList}
+          style={styles.flatList}
+          contentContainerStyle={styles.flatListContainer}
           data={filteredDishes}
-          renderItem={({ item }) => {
-            return (
-              <Pressable onPress={() => router.push(`/details/${item._id}`)}>
-                <View style={styles.card}>
-                  <Text style={styles.text} key={item._id}>
-                    {item.title[
-                      selectedLanguage?.id as keyof typeof item.title
-                    ] ?? item.title.es}
-                  </Text>
-
-                  <Animated.Image
-                    source={{ uri: item.imageUrl }}
-                    style={styles.backgroundImageCard}
-                    sharedTransitionTag={"dishImage"}
-                  />
-                </View>
-              </Pressable>
-            );
-          }}
+          renderItem={({ item }) => (
+            <Pressable
+              onPress={() =>
+                router.push(`/details/${item._id}` as "/details/:id")
+              }
+            >
+              <View style={styles.card}>
+                <Text style={styles.text} key={item._id}>
+                  {item.title[
+                    selectedLanguage?.id as keyof typeof item.title
+                  ] ?? item.title.es}
+                </Text>
+                <Animated.Image
+                  source={{ uri: item.imageUrl }}
+                  style={styles.backgroundImageCard}
+                  sharedTransitionTag={"dishImage"}
+                />
+              </View>
+            </Pressable>
+          )}
           keyExtractor={(item) => item._id}
           ListEmptyComponent={
             <Text style={{ color: theme?.text }}>No dishes found</Text>
@@ -141,11 +99,40 @@ export default function MenuScreen() {
   );
 }
 
-const createStyles = (theme: typeof Colors.light | undefined = Colors.light) =>
+const createStyles = (theme = Colors.light) =>
   StyleSheet.create({
     pageContainer: {
       flex: 1,
       backgroundColor: theme.background,
+    },
+    categoriesWrapper: {
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    categoriesList: {
+      backgroundColor: theme?.tint,
+      height: 100,
+      paddingVertical: 20,
+      borderBottomColor: "black",
+      borderBottomWidth: 2,
+    },
+    categoriesContainer: {
+      width: "100%",
+      alignItems: "center",
+      justifyContent: "space-around",
+    },
+    categoryButton: {
+      padding: 15,
+      backgroundColor: theme?.background,
+      minWidth: 100,
+      alignItems: "center",
+      justifyContent: "center",
+      borderRadius: 10,
+    },
+    categoryText: {
+      color: theme?.text,
+      textAlign: "center",
+      fontSize: 20,
     },
     listContainer: {
       flex: 1,
@@ -154,14 +141,17 @@ const createStyles = (theme: typeof Colors.light | undefined = Colors.light) =>
       alignItems: "center",
     },
     flatList: {
+      width: "100%",
+    },
+    flatListContainer: {
       padding: 50,
       justifyContent: "flex-start",
       alignItems: "flex-start",
       gap: 50,
     },
     card: {
-      width: 550,
-      height: 300,
+      width: CARD_WIDTH,
+      height: CARD_HEIGHT,
       borderRadius: 10,
       backgroundColor: theme?.icon,
       flex: 1,
