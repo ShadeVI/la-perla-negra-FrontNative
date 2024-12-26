73e6e50b32ed0c506271accc4bcbb78fb110b6d3
Manuel <manuel.t.shade@gmail.com>
2024-12-26 15:52:28 +0000

    feat: extracting theme and details; preparing the UI

--- app/details/[id].tsx
+++ app/details/[id].tsx
@@ -1,53 +1,58 @@
 import { useDishes } from "@/context/Dishes";
+import { useLanguage } from "@/context/Language";
+import { useTheme } from "@/context/Theme";
 import { useLocalSearchParams } from "expo-router";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { Dimensions, ScrollView, StyleSheet, Text, View } from "react-native";
 import Animated from "react-native-reanimated";
 
 const Details = () => {
   const params = useLocalSearchParams();
   const { dishes } = useDishes();
+  const { selectedLanguage } = useLanguage();
+  const { theme } = useTheme();
 
   const detailsDish = dishes.find((dish) => dish._id === params.id);
 
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "flex-start",
-        width: "100%",
-      }}
+    <ScrollView
+      style={{ flex: 1, width: "100%", backgroundColor: theme?.background }}
     >
-      <ScrollView style={{ flex: 1, width: "100%" }}>
-        <View style={{ width: "100%" }}>
-          <Animated.Image
-            source={{ uri: detailsDish?.imageUrl }}
-            style={{
-              width: "100%",
-              height: 500,
-              objectFit: "cover",
-              marginHorizontal: "auto",
-            }}
-            sharedTransitionTag={"dishImage"}
-          />
-        </View>
-        <Text>HEHEHEHEHHEHE</Text>
-        <Text>HEHEHEHEHHEHE</Text>
-        <Text>HEHEHEHEHHEHE</Text>
-        <Text>HEHEHEHEHHEHE</Text>
-        <Text>HEHEHEHEHHEHE</Text>
-        <Text>HEHEHEHEHHEHE</Text>
-        <Text>HEHEHEHEHHEHE</Text>
-        <Text>HEHEHEHEHHEHE</Text>
-        <Text>HEHEHEHEHHEHE</Text>
-        <Text>HEHEHEHEHHEHE</Text>
-        <Text>HEHEHEHEHHEHE</Text>
-        <Text>HEHEHEHEHHEHE</Text>
-        <Text>HEHEHEHEHHEHE</Text>
-        <Text>HEHEHEHEHHEHE</Text>
-        <Text>HEHEHEHEHHEHE</Text>
-      </ScrollView>
-    </View>
+      <View style={{ width: "100%" }}>
+        <Animated.Image
+          source={{ uri: detailsDish?.imageUrl }}
+          style={{
+            width: "100%",
+            height: Dimensions.get("screen").height,
+            objectFit: "cover",
+            marginHorizontal: "auto",
+          }}
+          sharedTransitionTag={"dishImage"}
+        />
+      </View>
+      <View>
+        <Text
+          style={{
+            textAlign: "center",
+            fontSize: 30,
+            marginTop: 20,
+            color: theme?.text,
+          }}
+        >
+          {detailsDish?.dishNumber} -{" "}
+          {detailsDish?.title[selectedLanguage?.id ?? "es"]}
+        </Text>
+        <Text
+          style={{
+            textAlign: "justify",
+            fontSize: 25,
+            marginTop: 20,
+            color: theme?.text,
+          }}
+        >
+          {detailsDish?.description[selectedLanguage?.id ?? "es"]}
+        </Text>
+      </View>
+    </ScrollView>
   );
 };
 export default Details;
