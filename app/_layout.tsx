f7c3fbb2529c16878cfcc832b15c5555668c1c0c
ShadeVI <manueltomasino.mt@gmail.com>
2025-02-10 02:35:26 +0000

    feat: update splash screen configuration and assets for improved visibility

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,69 +2,58 @@ import { DataProvider } from "@/context/Data";
 import { LanguageProvider } from "@/context/Language";
 import { OrderProvider } from "@/context/Order";
 import { ThemeProvider } from "@/context/Theme";
-import { Stack, SplashScreen } from "expo-router";
+import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { useCallback, useEffect, useState } from "react";
-import { Appearance, View } from "react-native";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import { Appearance } from "react-native";
 import "react-native-reanimated";
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from "expo-router";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [appIsReady, setAppIsReady] = useState(false);
+  const [loaded, error] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    ...FontAwesome.font,
+  });
+
+  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
 
   useEffect(() => {
-    async function prepare() {
-      try {
-        // Pre-load fonts, make any API calls you need to do here
-        // await Font.loadAsync(Entypo.font);
-        // Artificially delay for two seconds to simulate a slow loading
-        // experience. Remove this if you copy and paste the code!
-        await new Promise((resolve) => setTimeout(resolve, 2000));
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        // Tell the application to render
-        setAppIsReady(true);
-      }
+    if (loaded) {
+      SplashScreen.hideAsync();
     }
+  }, [loaded]);
 
-    prepare();
-  }, []);
-
-  const onLayoutRootView = useCallback(() => {
-    if (appIsReady) {
-      // This tells the splash screen to hide immediately! If we call this after
-      // `setAppIsReady`, then we may see a blank screen while the app is
-      // loading its initial state and rendering its first pixels. So instead,
-      // we hide the splash screen once we know the root view has already
-      // performed layout.
-      SplashScreen.hide();
-    }
-  }, [appIsReady]);
-
-  if (!appIsReady) {
+  if (!loaded) {
     return null;
   }
 
+  return <RootLayoutNav />;
+}
+
+function RootLayoutNav() {
   return (
     <ThemeProvider initialColorScheme={Appearance.getColorScheme()}>
       <LanguageProvider>
         <DataProvider>
           <OrderProvider>
             <StatusBar style="auto" />
-            <View
-              style={{
-                flex: 1,
-              }}
-              onLayout={onLayoutRootView}
-            >
-              <Stack>
-                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-                <Stack.Screen name="+not-found" />
-              </Stack>
-            </View>
+            <Stack>
+              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+              <Stack.Screen name="+not-found" />
+            </Stack>
           </OrderProvider>
         </DataProvider>
       </LanguageProvider>
