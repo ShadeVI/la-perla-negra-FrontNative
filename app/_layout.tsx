6e75bfd0a25dfb74dc18ddc7e8da798eab20f120
ShadeVI <manueltomasino.mt@gmail.com>
2025-02-10 03:46:00 +0000

    refactor: comment out font loading logic for testing

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -23,10 +23,13 @@ export const unstable_settings = {
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
+  /* const [loaded, error] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
     ...FontAwesome.font,
-  });
+  }); */
+
+  const loaded = true;
+  const error = false;
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
